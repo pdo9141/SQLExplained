cea7874a97bab0b336744c94a1c05f919e2b3acf
Phillip <Phillip@WAYTOOAWESOME>
2016-11-01 17:54:31 -0700

    Paging

---
+++ ConsoleApplication1/Topics/Paging/Queries.sql
@@ -0,0 +1,25 @@
+﻿DECLARE @OrderId BIGINT = NULL,
+		@PageNumber INT = 1,
+		@PageSize INT = 10
+
+DECLARE @RowStart INT, @RowEnd INT
+SET @PageNumber = @PageNumber - 1
+SET @RowStart = @PageSize * @PageNumber + 1
+SET @RowEnd = @RowStart + @PageSize - 1
+
+WITH ResultCTE AS (
+	SELECT
+		ROW_NUMBER() OVER (ORDER BY Id) AS RowNumber,
+		Id,
+		AppId,
+		OrderId,
+		ClientId
+	FROM FakeTable WITH (NOLOCK)
+	WHERE OrderId = @OrderId
+)
+
+SELECT
+	TotalRows = (SELECT COUNT(0) FROM ResultCTE),
+	*
+FROM ResultCTE
+WHERE RowNumber >= @RowStart AND RowNumber <= @RowEnd
