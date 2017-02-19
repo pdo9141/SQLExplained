6eca8d1b165e9d7ad45b25dbc15294a70e12a612
pdo9141 <pdo9141@gmail.com>
2017-02-18 23:42:05 -0800

    Row_Number

---
+++ ConsoleApplication1/Topics/RowNumber/ReadMe.txt
@@ -0,0 +1,5 @@
+﻿01) Returns the sequential number of a row starting at 1
+02) ORDER BY clause is required
+03) PARTITION BY clause is optional
+04) When the data is partitioned, row number is reset to 1 when the partition changes
+05) One use case is to delete all duplicates from a table
---
+++ ConsoleApplication1/Topics/RowNumber/TableCreate.sql
@@ -0,0 +1,37 @@
+﻿USE TestDB
+--CREATE TABLE dbo.Employees
+--(
+--	Name VARCHAR(50) NOT NULL,
+--	Gender VARCHAR(20) NOT NULL,
+--	Salary INT NOT NULL
+--)
+
+--INSERT INTO Employees VALUES ('Mark', 'Male', 5000)
+--INSERT INTO Employees VALUES ('John', 'Male', 4500)
+--INSERT INTO Employees VALUES ('Pam', 'Female', 5500)
+--INSERT INTO Employees VALUES ('Sara', 'Female', 4000)
+--INSERT INTO Employees VALUES ('Todd', 'Male', 3500)
+--INSERT INTO Employees VALUES ('Mary', 'Female', 5000)
+--INSERT INTO Employees VALUES ('Ben', 'Male', 6500)
+--INSERT INTO Employees VALUES ('Jodi', 'Female', 7000)
+--INSERT INTO Employees VALUES ('Tom', 'Male', 5500)
+--INSERT INTO Employees VALUES ('Ron', 'Male', 5000)
+
+CREATE TABLE Employees
+(
+     ID INT,
+     FirstName NVARCHAR(50),
+     LastName NVARCHAR(50),
+     Gender NVARCHAR(50),
+     Salary INT
+)
+GO
+
+INSERT INTO Employees VALUES (1, 'Mark', 'Hastings', 'Male', 60000)
+INSERT INTO Employees VALUES (1, 'Mark', 'Hastings', 'Male', 60000)
+INSERT INTO Employees VALUES (1, 'Mark', 'Hastings', 'Male', 60000)
+INSERT INTO Employees VALUES (2, 'Mary', 'Lambeth', 'Female', 30000)
+INSERT INTO Employees VALUES (2, 'Mary', 'Lambeth', 'Female', 30000)
+INSERT INTO Employees VALUES (3, 'Ben', 'Hoskins', 'Male', 70000)
+INSERT INTO Employees VALUES (3, 'Ben', 'Hoskins', 'Male', 70000)
+INSERT INTO Employees VALUES (3, 'Ben', 'Hoskins', 'Male', 70000)
